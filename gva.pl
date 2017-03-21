b0a97cf857c5b7c0a27613c666d4881e96766ec1
James E Keenan <jkeenan@cpan.org>
2017-03-21 08:34:42 -0400

    Only capture columns present in header row.
    Clean up inline editorial note in two columns of one row.
    
    Start to store screenshots of program's development, commentary on
    development.

--- gva.pl
+++ gva.pl
@@ -1,8 +1,7 @@
 # perl
 use strict;
 use warnings;
-use Data::Dumper;
-#$Data::Dumper::Indent=1;
+use Data::Dumper;$Data::Dumper::Indent=1;
 use Cwd;
 use Text::CSV;
 
@@ -15,8 +14,9 @@ tract_fips,num_killed,num_incidents,city_or_county,state,tract_square_mileage,20
 =cut
 
 my $sourcedir = cwd();
+my $inputsdir = "$sourcedir/inputs";
 my $tractfile = 'gva_release_2015_grouped_by_tract.csv';
-my $f = "$sourcedir/$tractfile";
+my $f = "$inputsdir/$tractfile";
 
 my @rows;
 my $csv = Text::CSV->new ( { binary => 1 } )
@@ -24,7 +24,11 @@ my $csv = Text::CSV->new ( { binary => 1 } )
  
 open my $IN, '<', $f or die "Unable to open $f: $!";
 while ( my $row = $csv->getline( $IN ) ) {
-     push @rows, $row;
+    my @thisrow = @{$row}[0..9];
+    for my $idx ( 1, 2 ) {
+        $thisrow[$idx] =~ s/^(.*)\(.\)$/$1/;
+    }
+    push @rows, \@thisrow;
  }
 $csv->eof or $csv->error_diag();
 close $IN;
